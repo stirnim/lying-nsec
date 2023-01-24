2cb896ff7197e36c6b7e5557c355da9409d6d7ca
Daniel Stirnimann <daniel.stirnimann@switch.ch>
2023-01-24 11:00:41 +0100

    fix zone parsing

--- utils/extract-signed-domain.sh
+++ utils/extract-signed-domain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -i "\tDS\t" | awk '{print $1}' | grep -v ^\; | sed 's/\.$//' | tr A-Z a-z | uniq
+egrep "\sIN\s+DS\s" | awk '{print $1}' | grep -v ^\; | sed 's/\.$//' | tr A-Z a-z | uniq
